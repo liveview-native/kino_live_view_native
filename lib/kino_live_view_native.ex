a2db4c25ea9d08df27097dcf9b7e13a298448868
Nduati Kuria <012nkuria@gmail.com>
2023-05-06 19:56:34 +0200

    Fix smart cell and add example

--- lib/kino_live_view_native.ex
+++ lib/kino_live_view_native.ex
@@ -1,21 +1,57 @@
 defmodule KinoLiveViewNative do
+  use Kino.JS
+  use Kino.JS.Live
   use Kino.SmartCell, name: "LiveView Native"
 
-  def init(_attrs, ctx) do
-    {:ok, ctx}
+  @impl true
+  def init(attrs, ctx) do
+    {:ok, ctx,
+     editor: [
+       attribute: "code",
+       language: "elixir",
+       default_source: ~s[
+defmodule Server.HomeLive do
+  use Phoenix.LiveView, layout: {__MODULE__, :layout}
+  use LiveViewNative.LiveView
+
+  def layout(assigns) do
+    ~H"""
+      <%= @inner_content %>
+    """
+  end
+
+  @impl true
+  def render(%{platform_id: :swiftui} = assigns) do
+    ~Z"""
+    <Text modifiers={@native |> foreground_style(primary: {:color, :mint})}>
+      Hello from LiveView Native!
+    </Text>
+    """swiftui
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>Hello from LiveView!!</div>
+    """
+  end
+end
+]
+     ]}
+  end
+
+  @impl true
+  def handle_connect(ctx) do
+    {:ok, %{}, ctx}
   end
 
   @impl true
   def to_attrs(ctx) do
-    %{"text" => ctx.assigns.text}
+    %{}
   end
 
   @impl true
   def to_source(attrs) do
-    quote do
-      IO.puts(unquote(attrs["text"]))
-    end
-    |> Kino.SmartCell.quoted_to_string()
+    attrs["code"]
   end
 
   def scan_eval_result(_server, _eval_result) do
