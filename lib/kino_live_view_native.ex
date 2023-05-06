4f62476290c89e617b5805b2a1f0360a9f712235
Nduati Kuria <012nkuria@gmail.com>
2023-05-06 22:26:20 +0200

    Add dynamic paths

--- lib/kino_live_view_native.ex
+++ lib/kino_live_view_native.ex
@@ -2,15 +2,97 @@ defmodule KinoLiveViewNative do
   use Kino.JS
   use Kino.JS.Live
   use Kino.SmartCell, name: "LiveView Native"
+  require IEx.Helpers
 
   @impl true
   def init(attrs, ctx) do
-    {:ok, ctx,
+    {:ok,
+     ctx
+     |> assign(
+       path: attrs["path"] || "/",
+       action: attrs["action"] || ":index"
+     ),
      editor: [
        attribute: "code",
        language: "elixir",
-       default_source: ~s[
-defmodule Server.HomeLive do
+       default_source: default_source()
+     ]}
+  end
+
+  @impl true
+  def handle_connect(ctx) do
+    {:ok, %{path: ctx.assigns.path, action: ctx.assigns.action}, ctx}
+  end
+
+  @impl true
+  def to_attrs(ctx) do
+    %{
+      "path" => ctx.assigns.path,
+      "action" => ctx.assigns.action
+    }
+  end
+
+  @impl true
+  def to_source(attrs) do
+    [
+      attrs["code"],
+      "|>",
+      quote do
+        unquote(__MODULE__).register(unquote(attrs["path"]), unquote(attrs["action"]))
+      end
+      |> Kino.SmartCell.quoted_to_string()
+    ]
+  end
+
+  @impl true
+  def handle_event("update_" <> variable_name, value, ctx) do
+    variable = String.to_existing_atom(variable_name)
+    ctx = assign(ctx, [{variable, value}])
+
+    broadcast_event(
+      ctx,
+      "update_" <> variable_name,
+      ctx.assigns[variable]
+    )
+
+    {:noreply, ctx}
+  end
+
+  @registry_key :liveviews
+
+  def get_modules() do
+    Application.get_env(:kino_live_view_native, @registry_key, [])
+  end
+
+  defp put_modules(modules) do
+    Application.put_env(:kino_live_view_native, @registry_key, modules)
+  end
+
+  def register({:module, module, _, _}, path, action) do
+    modules = get_modules()
+
+    updated_modules =
+      if module in modules do
+        modules
+      else
+        action =
+          action
+          |> String.trim_leading(":")
+          |> String.to_atom()
+
+        modules ++ [%{path: path, module: module, action: action}]
+      end
+
+    put_modules(updated_modules)
+  end
+
+  def scan_eval_result(_server, _eval_result) do
+    Phoenix.PubSub.broadcast!(Server.PubSub, "reloader", :trigger)
+    IEx.Helpers.r(Server.Router)
+  end
+
+  def default_source() do
+    ~s[defmodule Server.HomeLive do
   use Phoenix.LiveView, layout: {__MODULE__, :layout}
   use LiveViewNative.LiveView
 
@@ -36,25 +118,83 @@ defmodule Server.HomeLive do
   end
 end
 ]
-     ]}
   end
 
-  @impl true
-  def handle_connect(ctx) do
-    {:ok, %{}, ctx}
+  asset "main.js" do
+    """
+    export function init(ctx, payload) {
+      ctx.importCSS("main.css");
+      ctx.importCSS("https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap");
+
+      root.innerHTML = `
+        <div class="app">
+          <label class="label">Route</label>
+          <input class="input" type="text" name="path" />
+
+          <label class="label">Action</label>
+          <input class="input" type="text" name="action" />
+        </div>
+      `;
+
+      const sync = (id) => {
+          const variableEl = ctx.root.querySelector(`[name="${id}"]`);
+          variableEl.value = payload[id];
+
+          variableEl.addEventListener("change", (event) => {
+            ctx.pushEvent(`update_${id}`, event.target.value);
+          });
+
+          ctx.handleEvent(`update_${id}`, (variable) => {
+            variableEl.value = variable;
+          });
+      }
+
+      sync("path")
+      sync("action")
+
+      ctx.handleSync(() => {
+          // Synchronously invokes change listeners
+          document.activeElement &&
+            document.activeElement.dispatchEvent(new Event("change"));
+      });
+    }
+    """
   end
 
-  @impl true
-  def to_attrs(ctx) do
-    %{}
-  end
+  asset "main.css" do
+    """
+    .app {
+      font-family: "Inter";
+      display: flex;
+      align-items: center;
+      gap: 16px;
+      background-color: #ecf0ff;
+      padding: 8px 16px;
+      border: solid 1px #cad5e0;
+      border-radius: 0.5rem 0.5rem 0 0;
+    }
 
-  @impl true
-  def to_source(attrs) do
-    attrs["code"]
-  end
+    .label {
+      font-size: 0.875rem;
+      font-weight: 500;
+      color: #445668;
+      text-transform: uppercase;
+    }
 
-  def scan_eval_result(_server, _eval_result) do
-    Phoenix.PubSub.broadcast!(Server.PubSub, "reloader", :trigger)
+    .input {
+      padding: 8px 12px;
+      background-color: #f8f8afc;
+      font-size: 0.875rem;
+      border: 1px solid #e1e8f0;
+      border-radius: 0.5rem;
+      color: #445668;
+      min-width: 150px;
+    }
+
+    .input:focus {
+      border: 1px solid #61758a;
+      outline: none;
+    }
+    """
   end
 end
