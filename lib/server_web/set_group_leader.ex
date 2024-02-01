e6bc08b265b51889afa2a3bfecde81f46a035d13
BrooklinJazz <brooklinjazzmyers@gmail.com>
2024-02-01 12:47:56 -0500

    document group leader on_mount callback

--- lib/server_web/set_group_leader.ex
+++ lib/server_web/set_group_leader.ex
@@ -1,8 +1,12 @@
 defmodule ServerWeb.SetGroupLeader do
-  require Logger
+  @moduledoc """
+  Set the group leader for the current process to the one specified in the
+  kino Livebook configuration. This allows IO.inspect logs to be viewed in Livebook
+  instead of the terminal.
+  """
 
   def on_mount(_, _params, _session, socket) do
-    Process.group_leader(self(), Application.get_env(:kino, :group_leader))
+    Process.group_leader(self(), Application.get_env(:kino, :group_leader) || Process.group_leader())
     {:cont, socket}
   end
 end
