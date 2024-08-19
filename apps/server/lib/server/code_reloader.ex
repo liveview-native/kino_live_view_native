5713b350b5b1107c981990a498327f89294412e1
BrooklinJazz <brooklinjazzmyers@gmail.com>
2024-08-18 20:53:22 -0400

    Fix {:error, "nofile"} issue caused by code_reloader

--- apps/server/lib/server/code_reloader.ex
+++ apps/server/lib/server/code_reloader.ex
@@ -1,14 +1,9 @@
 defmodule Server.CodeReloader do
   def reload!(_, _) do
-    # This transaction ensures that this function is only called once during the code reload.
-    # This change was due to an error where the web app.js would not load properly and web event handlers were broken
-    :global.trans({:resource_id, :lock_requester_id}, fn ->
-      Mix.in_install_project(fn ->
-        # Fixed in future Hex, but needed for now
-        _ = Application.ensure_all_started(:hex)
-        Mix.Task.rerun("deps.compile", ["kino_live_view_native"])
-        Mix.Task.clear()
-      end)
-    end)
+    # Calling `Mix.Project.app_path()` when Phoenix from Livebook produces the following error:
+    # Cannot access build without an application name, please ensure you are in a directory with a mix.exs file and it defines an :app name under the project configuration
+    # This function is called by `Mix.Project.consolidation_paths/1` when recompiling because `Mix.Project.umbrella?/1` returns false.
+    # This custom code reloader hides the error, though it will still print in the Livebook console, but not in Livebook code cells.
+    :ok
   end
 end
